e14111eb89f9e81e8a40919cbf39cd766814c464
lbodnyk-dcls-org <lbodnyk@dcls.org>
2017-04-03 16:45:41 -0400

    trying to create an editable text field

--- app/src/main/java/com/example/lbodnyk/elapsed/MainActivity.java
+++ app/src/main/java/com/example/lbodnyk/elapsed/MainActivity.java
@@ -9,12 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.util.Log;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import static android.text.format.DateUtils.getRelativeTimeSpanString;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,20 +41,22 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
            Snackbar.make(view, "Trying to add a new MyElapsedTimeObject", Snackbar.LENGTH_LONG)
                    .setAction("Action", null).show();
-           myArrayOfElapsedTimeObjects.add(new MyElapsedTimeObject("test", Calendar.getInstance().getTimeInMillis()));
+
+
+           myArrayOfElapsedTimeObjects.add(new MyElapsedTimeObject("new", Calendar.getInstance().getTimeInMillis()));
 
         }
         });
 
         // for testing, prepopulate with some timers.
         myArrayOfElapsedTimeObjects.add(new MyElapsedTimeObject("test", Calendar.getInstance().getTimeInMillis()));
+
         try {
             Thread.sleep(100); // Waits for 1 second (1000 milliseconds)
         } catch (InterruptedException e) {
             System.out.println("I was interrupted!");
             e.printStackTrace();
         }
-        //myArrayOfElapsedTimeObjects.add(new MyElapsedTimeObject("test", Calendar.getInstance().getTimeInMillis()));
 
         // Create the adapter to convert the array to views
         final ElapsedTimeObjectArrayAdapter arrayAdapter = new ElapsedTimeObjectArrayAdapter(this, myArrayOfElapsedTimeObjects);
@@ -60,35 +64,75 @@ public class MainActivity extends AppCompatActivity {
 
         final Handler myHandler = new Handler();
 
-        Runnable myRunnable = new Runnable() {
-            @Override
+        // {
+        class MainActivityRunnable implements Runnable {
+            final private Object selfPauseLock;
+            private boolean selfPaused;
+            private boolean selfFinished;
+
+            private MainActivityRunnable() {
+                selfPauseLock = new Object();
+                selfPaused = false;
+                selfFinished = false;
+            }
+
             public void run() {
-                int loops = 0;
-                boolean exitThread = false;
-                while (!exitThread) {
+                while (!selfFinished) {
                     try {
                         Thread.sleep(1000); // Waits for 1 second (1000 milliseconds)
                     } catch (InterruptedException e) {
-                        System.out.println("I was interrupted!");
+                        System.out.println("I am required to inform you that I was interrupted!");
                         e.printStackTrace();
                     }
-                    loops += 1;
-                    if (loops == 90) {
-                        exitThread = true;
-                    }
-                    final int thisiswhateveriwant = loops;
                     myHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            manualElapsedSeconds += 1;
-                            arrayAdapter.notifyDataSetChanged();
+                            if (!findViewById(R.id.elapsedtimelistitemtitle).hasFocus()) {
+                                arrayAdapter.notifyDataSetChanged();
+                                Log.d(MAIN, "NOT Editing text!");
+                            }
                             Log.d(MAIN, "calculatedElapsedTime: " + (getRelativeTimeSpanString(timeAtOnCreate, Calendar.getInstance().getTimeInMillis(), 1000 )).toString());
                         }
                     });
+                    synchronized (selfPauseLock) {
+                        while (selfPaused) {
+                            try {
+                                selfPauseLock.wait();
+                            } catch (InterruptedException e) {
+                            }
+                        }
+                    }
                 }
             }
-        };
 
+            public void onPause() {
+                synchronized (selfPauseLock) {
+                    selfPaused = true;
+                }
+            }
+
+            public void onResume() {
+                synchronized (selfPauseLock) {
+                    selfPaused = false;
+                    selfPauseLock.notifyAll();
+                }
+            }
+        }
+
+/*        View.OnFocusChangeListener focusListener = new View.OnFocusChangeListener() {
+            View focusedView;
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    focusedView = v;
+                } else {
+                    focusedView  = null;
+                }
+            }
+        };*/
+
+        //findViewById(R.id.elapsedtimelistitemtitle).setOnFocusChangeListener(focusListener);
+
+        Runnable myRunnable = new MainActivityRunnable();
         Thread myThread = new Thread(myRunnable);
         myThread.start();
     }
